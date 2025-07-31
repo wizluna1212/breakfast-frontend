9383d703a2d9353e628e39e62ae49b3fe84fc5b3
wizluna1212 <wizluna1212@gmail.com>
2025-08-01 01:43:49 +0800

    修正el樣式無法正確顯示的問題

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import App from './App.vue'
 import router from './router'
 import './assets/main.css'
 import './assets/main.scss'
+import 'element-plus/es/components/message/style/css'
 
 const app = createApp(App)
 const pinia = createPinia()
